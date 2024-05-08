1ac39da9a2316c91f38bf4c5cbd97de3c506f267
Alessandro <alessandrojosue53@gmail.com>
2024-05-07 21:33:36 -0600

    Proyecto terminado

--- js/app.js
+++ js/app.js
@@ -6,19 +6,61 @@ document.addEventListener('DOMContentLoaded', function(){
         mensaje: ''
     }
 
-
     //Seleccionamos los elementos de la interfaz
     const inputEmail = document.querySelector('#email');
     const inputAsunto = document.querySelector('#asunto');
     const inputMensaje = document.querySelector('#mensaje')
     const formulario = document.querySelector('#formulario')
     const btnSubmit = document.querySelector('#formulario button[type="submit"]')
+    const btnReset = document.querySelector('#formulario button[type="reset"]')
+    const spinner = document.querySelector('#spinner');
 
     //Asignamos eventos
     //El blur sirve cuando un usuario ingresa al input y no escribe nada este avisa (por ejemplo: no puede quedar vacio).
-    inputEmail.addEventListener('blur',  validar)
-    inputAsunto.addEventListener('blur', validar)
-    inputMensaje.addEventListener('blur', validar)
+    inputEmail.addEventListener('input',  validar)
+    inputAsunto.addEventListener('input', validar)
+    inputMensaje.addEventListener('input', validar)
+
+    formulario.addEventListener('submit',enviarEmail);
+
+    btnReset.addEventListener('click', function(e){
+        e.preventDefault();
+
+
+    })
+
+    function enviarEmail(e){
+        e.preventDefault();
+
+        console.log('enviando...')
+
+        spinner.classList.add('flex');
+        spinner.classList.remove('hidden');
+
+
+        setTimeout(() =>{
+            spinner.classList.remove('flex');
+            spinner.classList.add('hidden');
+
+            resetFormulario();
+
+            //crear una alerta
+            const alertaExito = document.createElement('P');
+            alertaExito.classList.add('bg-green-500', 'text-white','p-2','text-center','rounded-lg','mt-10','font-bold','text-sm','uppercase');
+            alertaExito.textContent='Mensaje enviado correctamente';
+
+            formulario.appendChild(alertaExito);
+
+            setTimeout(() =>{
+                alertaExito.classList.add('hidden');
+            }, 3000)
+        }, 3000)
+
+        exitoso = createElement('p');
+        exitoso.textContent = 'Datos enviados exitosamente';
+        exitoso.classList.add('text-green')
+
+    }   
 
     function validar(e){
         if (e.target.value.trim() === ''){
@@ -41,9 +83,7 @@ document.addEventListener('DOMContentLoaded', function(){
 
         //comprobar el objeto email
         comprobarEmail();
-
     }
-
 
     function mostrarAlerta(mensaje, referencia) {
         //Comprueba que ya existe una alerta
@@ -82,6 +122,17 @@ document.addEventListener('DOMContentLoaded', function(){
         }
             btnSubmit.classList.remove('opacity-50');
             btnSubmit.disabled = false;
+    }
+
+    function resetFormulario(){
+
+        //reiniciar el objeto
+        email.email = '';
+        asunto.asunto = '';
+        mensaje.mensaje = '';
+        formulario.reset();
+        comprobarEmail();
+
     }
 });
 
